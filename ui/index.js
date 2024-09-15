02bcf3fe8c1b0b7c27e39c74be23202b65850518
hchiam <hchiam@users.noreply.github.com>
2024-09-15 11:16:12 -0600

    minimal bug recreated

--- ui/index.js
+++ ui/index.js
@@ -1,2 +1,22 @@
-console.log('hi');
-alert('hi')
+class Demo {
+    constructor() {
+        this.a = 'a'
+        this.#b = '#b'
+        this.c();
+    }
+
+    #b = 1
+
+    c() {
+        out.value = `${this.a}, ${this.#b}, c`
+        return 'c'
+    }
+
+    /** commenting out private method #d() gets rid of the console error */
+    #d() {
+        out.value = `${this.a}, ${this.#b}, c, #d`
+        return '#d'
+    }
+}
+
+demo = new Demo()
